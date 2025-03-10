943ff1d90571d050d872b99dc4698a9db57153a4
Julien Midedji <Julien.Midedji@gmail.com>
2025-03-10 18:51:37 +0100

    Setup fundamentals for rendering

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"net/http"
+	"text/template"
 
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
 func main() {
-	fmt.Println("Hello World")
+	t := &Template{
+		templates: template.Must(template.ParseGlob("www/index.html")),
+	}
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Renderer = t
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/render", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "hello", "world")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+type Template struct {
+    templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
