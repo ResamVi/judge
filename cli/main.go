8e4077df71735d5926b2b2aee32b7396cb346899
Julien Midedji <Julien.Midedji@inovex.de>
2025-10-28 20:36:03 +0100

    Upload code

--- cli/main.go
+++ cli/main.go
@@ -13,7 +13,8 @@ import (
 
 	"encoding/json"
 	"github.com/alecthomas/kong"
-	
+
+	"github.com/kirsle/configdir"
 )
 
 const (
@@ -27,30 +28,36 @@ var CLI struct {
 	} `cmd:"" name:"configure" help:"Kopiere dein Token um Code hochladen und herunterladen zu können"`
 
 	Download struct {
-		Aufgabe bool `arg:"" name:"aufgabe" help:"Nummer der Aufgabe die gedownloadet werden soll."`
+		Aufgabe string `arg:"" name:"aufgabe" help:"Nummer der Aufgabe die gedownloadet werden soll."`
 	} `cmd:"" name:"download" help:"Downloade eine Aufgabe"`
 
 	Upload struct {
-		Aufgabe bool   `arg:"" name:"aufgabe" help:"Nummer der Aufgabe für die eine Lösung hochgeladen wird."`
+		Aufgabe string `arg:"" name:"aufgabe" help:"Nummer der Aufgabe für die eine Lösung hochgeladen wird."`
 		Ordner  string `arg:"" name:"ordner" help:"Name des Ordners der hochgeladen werden soll" type:"existingdir"`
 	} `cmd:"" name:"upload" help:"Upload deiner Lösung für die Aufgabe"`
 
 	Review struct {
-		Aufgabe  bool   `help:"Nummer der Aufgabe für die eine Lösung hochgeladen wird."`
+		Aufgabe  string `help:"Nummer der Aufgabe für die eine Lösung hochgeladen wird."`
 		Benutzer string `help:"Nutzername der Person die Lösung hochgeladen hat"`
 	} `cmd:"" name:"review" help:"Downloade eine Aufgabe"`
 }
 
 func main() {
-	ctx := kong.Parse(&CLI)
+	ctx := kong.Parse(&CLI, kong.Name("judge-cli"),
+		kong.Description("Zum downloaden, uploaden und anschauen von Übungen"),
+		kong.UsageOnError(),
+		kong.ConfigureHelp(kong.HelpOptions{
+			Compact: true,
+			Summary: true,
+		}))
 
 	switch ctx.Command() {
 	case "configure <token>":
 		configure()
 	case "download":
-		download()
-	case "upload":
-		upload()
+		download(CLI.Download.Aufgabe)
+	case "upload <aufgabe> <ordner>":
+		upload(CLI.Upload.Aufgabe, CLI.Upload.Ordner)
 	case "review":
 		// Todo: downloading other submissions
 	default:
@@ -84,61 +91,30 @@ func configure() {
 	fmt.Println("Created a config at " + configPath)
 }
 
-func upload() {
+func upload(aufgabe string, ordner string) {
 	token := loadConfig()
 
-	exerciseNo := os.Args[2]
-
-	if _, err := os.Stat(exerciseNo); os.IsNotExist(err) {
-		panic("folder does not exist: " + exerciseNo) // TODO: NO panics
+	if _, err := os.Stat(ordner); os.IsNotExist(err) {
+		panic("folder does not exist: " + ordner) // TODO: NO panics
 	}
 
-	var buf bytes.Buffer
-	w := zip.NewWriter(base64.NewEncoder(base64.StdEncoding, &buf))
-
-	walker := func(path string, info os.FileInfo, err error) error {
-		fmt.Printf("Crawling: %#v\n", path)
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			return nil
-		}
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		// Ensure that `path` is not absolute; it should not start with "/".
-		// This snippet happens to work because I don't use
-		// absolute paths, but ensure your real-world code
-		// transforms path into a zip-root relative path.
-		f, err := w.Create(path)
-		if err != nil {
-			return err
-		}
-
-		_, err = io.Copy(f, file)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}
-	err := filepath.Walk(exerciseNo, walker)
+	// 1. Zip the folder
+	zipBytes, err := zipDirectory(ordner)
 	if err != nil {
 		panic(err)
 	}
-	w.Close()
 
-	req, err := http.NewRequest(http.MethodPost, urlSubmit, &buf)
+	// 2. Base64-encode zip
+	b64 := base64.StdEncoding.EncodeToString(zipBytes)
+
+	// 4. POST it
+	req, err := http.NewRequest(http.MethodPost, urlSubmit, bytes.NewReader([]byte(b64)))
 	if err != nil {
 		panic(err)
 	}
 	req.Header.Set("token", token)
-	req.Header.Set("exercise", exerciseNo)
-	req.Header.Set("Content-Type", "application/zip")
+	req.Header.Set("exercise", aufgabe)
+	req.Header.Set("Content-Type", "text/plain")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -152,17 +128,15 @@ func upload() {
 	}
 }
 
-func download() {
+func download(aufgabe string) {
 	//token := loadConfig()
 
-	exerciseNo := os.Args[2]
-
-	err := os.Mkdir(exerciseNo, os.ModePerm)
+	err := os.Mkdir(aufgabe, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
 
-	resp, err := http.Get(fmt.Sprintf(url, exerciseNo))
+	resp, err := http.Get(fmt.Sprintf(url, aufgabe))
 	if err != nil {
 		panic(err)
 	}
@@ -188,7 +162,7 @@ func download() {
 			continue
 		}
 
-		out, err := os.Create(exerciseNo + "/" + zipFile.Name)
+		out, err := os.Create(aufgabe + "/" + zipFile.Name)
 		if err != nil {
 			panic(err)
 		}
@@ -199,7 +173,6 @@ func download() {
 
 		out.Close()
 	}
-
 }
 
 type Config struct {
@@ -238,4 +211,67 @@ func readZipFile(zf *zip.File) ([]byte, error) {
 	}
 	defer f.Close()
 	return io.ReadAll(f)
+}
+
+// zipDirectory walks dirPath and writes a .zip (in-memory) with full structure.
+// Returns the raw bytes of the zip file.
+func zipDirectory(dirPath string) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	zipWriter := zip.NewWriter(buf)
+
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, walkErr error) error {
+		if walkErr != nil {
+			return walkErr
+		}
+
+		// Build the path inside the zip (relative to dirPath)
+		relPath, err := filepath.Rel(dirPath, path)
+		if err != nil {
+			return err
+		}
+
+		// Skip the root itself ("."), but still include its children
+		if relPath == "." {
+			return nil
+		}
+
+		// Directories in zip need a trailing slash
+		if info.IsDir() {
+			_, err := zipWriter.Create(relPath + "/")
+			return err
+		}
+
+		// It's a file: create header, copy contents
+		fileHeader, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+		fileHeader.Name = relPath
+		fileHeader.Method = zip.Deflate
+
+		writer, err := zipWriter.CreateHeader(fileHeader)
+		if err != nil {
+			return err
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		_, err = io.Copy(writer, f)
+		return err
+	})
+
+	if err != nil {
+		zipWriter.Close()
+		return nil, err
+	}
+
+	if err := zipWriter.Close(); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
--- handler/submit.go
+++ handler/submit.go
@@ -1,37 +1,104 @@
 package handler
 
 import (
+	"archive/zip"
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"fmt"
 	"encoding/base64"
 	"github.com/labstack/echo/v4"
 	"io"
 	"log/slog"
-	"net/http"
-	"os"
 	"time"
+	"archive/zip"
+	"time"
+	"fmt"
+	"bytes"
 )
 
 func (k Handler) Submit(c echo.Context) error {
-	token := c.Request().Header.Get("token")
-	exercise := c.Request().Header.Get("exercise")
+	//token := c.Request().Header.Get("token")
+	//exercise := c.Request().Header.Get("exercise")
 
-	user, err := k.db.GetUserFromToken(c.Request().Context(), token)
+	//user, err := k.db.GetUserFromToken(c.Request().Context(), token)
+	//if err != nil {
+	//	slog.Error("could not find user with token", "token", token, "err", err)
+	//	return c.String(http.StatusNotFound, err.Error())
+	//}
+
+	// Decode base64
+	data, err := io.ReadAll(base64.NewDecoder(base64.StdEncoding, c.Request().Body))
 	if err != nil {
-		slog.Error("could not find user with token", "token", token, "err", err)
-		return c.String(http.StatusNotFound, err.Error())
+		slog.Error("bad base64", "error", err.Error())
+		return c.String(http.StatusBadRequest, err.Error())
 	}
 
-	// todo: add exercise no and attempt count
-	f, err := os.Create("submissions/" + time.Now().Format("2006-01-02T15-04") + "_" + user.Username + exercise + ".zip")
-	if err != nil {
-		slog.Error("could not create file in submission folder", "err", err)
-		return c.String(http.StatusInternalServerError, err.Error())
+	// Destination path
+	destDir := filepath.Join("submissions", time.Now().Format("2006-01-02T15-04"))
+
+	if err := os.MkdirAll(destDir, os.ModePerm); err != nil {
+		slog.Error("cannot create dest dir", "error", err.Error())
+		return c.String(http.StatusBadRequest, err.Error())
 	}
 
-	_, err = io.Copy(f, base64.NewDecoder(base64.StdEncoding, c.Request().Body))
-	if err != nil {
-		slog.Error("could not copy http body to file", "err", err)
+	if err := unzipBytes(data, destDir); err != nil {
+		slog.Error("unzip failed", "error", err)
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
 	return c.String(http.StatusOK, ":)")
+}
+
+// unzipBytes extracts a zip-from-memory into destDir.
+// It will create destDir if it does not exist.
+func unzipBytes(zipData []byte, destDir string) error {
+	readerAt := bytes.NewReader(zipData)
+	zr, err := zip.NewReader(readerAt, int64(len(zipData)))
+	if err != nil {
+		return err
+	}
+
+	for _, f := range zr.File {
+		targetPath := filepath.Join(destDir, f.Name)
+
+		// SECURITY: block Zip Slip (`../` escape)
+		if !strings.HasPrefix(filepath.Clean(targetPath), filepath.Clean(destDir)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path in zip: %s", f.Name)
+		}
+
+		if f.FileInfo().IsDir() {
+			if err := os.MkdirAll(targetPath, os.ModePerm); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// Ensure parent directory exists
+		if err := os.MkdirAll(filepath.Dir(targetPath), os.ModePerm); err != nil {
+			return err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer rc.Close()
+
+		outFile, err := os.Create(targetPath)
+		if err != nil {
+			return err
+		}
+
+		if _, err := io.Copy(outFile, rc); err != nil {
+			outFile.Close()
+			return err
+		}
+
+		if err := outFile.Close(); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
